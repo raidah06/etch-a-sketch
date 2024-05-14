5dbe65cd808ee27a40ab1eff81a2af0c2c445f07
raid <muhammadraid2006@gmail.com>
2024-05-14 21:34:50 +0800

    added a sizing and color option

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 const GRIDSIDE = 600;
 let rows = 16;
 let cols = 16;
+let clr="blue"
 
 
 const sketchArea = document.querySelector("#sketch-area");
+
+
+
 sketchArea.style.width = `${GRIDSIDE}px`;
 sketchArea.style.height = `${GRIDSIDE}px`;
 
@@ -16,16 +20,43 @@ function createGridCells() {
         gridCell.classList.add("cell");
 
         sketchArea.appendChild(gridCell);
-
+  
         function addHovermouse() {
             gridCell.addEventListener("mouseover", (event) => {
-              
-                event.target.style.backgroundColor ="red";
+                
+                event.target.style.backgroundColor =clr;
             })
         }
         addHovermouse()
     }
+    function changeColor(){
+        clr=prompt("Type the color you need")
+        gridCell.addEventListener("mouseover", (event) => {
+                
+            event.target.style.backgroundColor =clr;
+        })
+    
+    }
+    colorButton.addEventListener("click",changeColor);
+
 }
+const gridButton=document.querySelector("#gridButton");
+function change(){
+    let changer=0
+    do{
+        changer=prompt("How many square do you want per line?");
+    }
+    while(changer>150);
+    rows=changer;
+    cols=changer;
+    document.querySelectorAll('.cell').forEach(e => e.remove());
+    createGridCells()
+    return rows,cols
+}
+const colorButton=document.querySelector('#colorButton')
+
+gridButton.addEventListener("click",change)
+
 
 createGridCells();
 
